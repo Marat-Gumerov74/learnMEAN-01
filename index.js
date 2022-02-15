4effe316d1a1f66b9e1701d8fc63932fa211ce80
Marat Gumerov <gumerovm74@gmail.com>
2022-02-15 23:32:24 +0300

    add basic pages and connect

--- index.js
+++ index.js
@@ -4,11 +4,38 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const path = require('path');
+const account = require('./routes/account');
+
+//add config
+const config = require('./config/db');
 
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, (req, res) => {
+//add cors
+app.use(cors());
+
+//add bodyparser
+app.use(bodyParser.json());
+
+//mongoose connection
+mongoose.connect(config.db, { useNewUrlParser: true});
+mongoose.connection.on('connected', () => {
+    console.log(`Successfull conection to the DB`);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(`Not Successfull conection to the DB + ${err}`);
+});
+
+//route
+app.get('/', (req, res) => {
+    res.send(`Home page`)
+});
+
+app.use('/account', account);
+
+app.listen(PORT, () => {
     console.log(`Server started on ${PORT} - port`);
 });
 
---
+++ routes/account.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/reg', (req, res) => {
+    res.send(`Registration page`)
+});
+
+router.get('/auth', (req, res) => {
+    res.send(`Login page`)
+});
+
+router.get('/dashboard', (req, res) => {
+    res.send(`Dashboard`)
+});
+
+module.exports = router;
